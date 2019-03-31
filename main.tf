627539022a7b4af36bf5fb7a4ad7cde23131749b
nguyenvandan <nguyenvandan@gmail.com>
2019-03-31 17:13:06 +0200

    reforme code

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "rabbitmq-instance" {
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "t2.micro"
+  key_name = "${aws_key_pair.rabbitmq-key.key_name}"
+
   security_groups = [
     "${aws_security_group.allow_ssh.name}",
     "${aws_security_group.allow_outbound.name}"
   ]
-  key_name = "${aws_key_pair.rabbitmq-key.key_name}"
 
   tags {
     Name = "rabbitmq-instance"
