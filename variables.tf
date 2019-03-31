a81f7bf3bc96ec93523cf084e0e9a09254bbe572
nguyenvandan <nguyenvandan@gmail.com>
2019-03-31 09:11:24 +0200

    init rabbitmq ec2 instance

---
+++ variables.tf
@@ -0,0 +1,13 @@
+# AWS Config
+
+variable "aws_access_key" {
+  default = "AKIA4VDR3OKXEWUOZKEF"
+}
+
+variable "aws_secret_key" {
+  default = "99PNdFdUrxIuXJWAPCXlWuXpUDyGrpVMV0rOk35j"
+}
+
+variable "aws_region" {
+  default = "eu-west-1"
+}
