8b59c891f1ecfa09847628f12389c51dda47b217
nguyenvandan <nguyenvandan@gmail.com>
2019-04-02 14:04:11 +0200

    tfvars

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "aws_key_pair" "my-rabbitmq-key" {
   key_name   = "my_rabbitmq_key"
-  public_key = "${file("id_rsa.pub")}"
+  public_key = "${file("C:\\Users\\vanguyen\\.ssh\\id_rsa.pub")}"
 }
 
 module "my-rabbitmq-server" {
   source = "rabbitmq-server"
 
-  name            = "rabbitmq-instance"
+  name            = "rabbitmq-server"
   key_pair        = "${aws_key_pair.my-rabbitmq-key.key_name}"
-  key_pair_key    = "~/.ssh/id_rsa"
+  key_pair_key    = "C:\\Users\\vanguyen\\.ssh\\id_rsa"
   security_groups = [
     "${aws_security_group.allow_ssh.name}",
     "${aws_security_group.allow_outbound.name}"
